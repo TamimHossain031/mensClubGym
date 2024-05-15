2a942a74e50ce81bc90f7dc235b7b8b9c7689242
Tamim <tamimhossain031@gmail.com>
2024-05-15 13:37:40 +0600

    add

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,18 +1,23 @@
-export default function Footer(){
-    return (
-            <section className="flex justify-between px-4">	
-                <div className="text-white">
-                <h1 className="text-sky-500 text-xl"> Men<span className="text-white">'</span>s Club <span className="text-[30px] text-white">.</span></h1>
-                <h1 className="text-white">Gym</h1>
-                <p>Akmal Ali Road</p>
-                <p>Epz,Chattogram</p>
-                </div>
-                <div className="text-white">
-                    <h1>Contact</h1>
-                    <p>Phone : 01677524484</p>
-                    <p>Email:mdmipukc@gmail.com</p>
+export default function Footer() {
+  return (
+    <section className="flex justify-between px-4 py-4">
+      <div className="text-white">
+        <h1 className="text-sky-500 text-md">
+          {" "}
+          Men<span className="text-white">'</span>s Club{" "}
+          <span className="text-[30px] text-white">.</span>
+        </h1>
 
-                </div>
-            </section>
-    );
+        <p className="text-white text-xs">Akmal Ali Road</p>
+        <p className="text-white text-xs">Epz,Chattogram</p>
+      </div>
+      <div className="text-white text-xs">
+        <h1>Contact</h1>
+        <p>Phone :</p>
+        <p>01677524484</p>
+        <p>Email:</p>
+        <p>mdmipukc@gmail.com</p>
+      </div>
+    </section>
+  );
 }
